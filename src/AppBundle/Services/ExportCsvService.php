5c7164e0029d5c7af2d344ed09b0bfe686f6bdcb
ncꙬky - nukegaming® <nugraha.saputra@gmail.com>
2021-08-01 17:56:16 +0700

    adding comment

--- src/AppBundle/Services/ExportCsvService.php
+++ src/AppBundle/Services/ExportCsvService.php
@@ -13,6 +13,8 @@ class ExportCsvService
             "order_id", "order_datetime", "total_order_value", "average_unit_price",
             "distinct_unit_count", "total_units_count", "customer_state"
         );
+
+        // put data to file
         fputcsv($fp, $row);
         
         foreach ($arr as $data) {
--- src/AppBundle/Services/ExportXmlService.php
+++ src/AppBundle/Services/ExportXmlService.php
@@ -7,8 +7,10 @@ class ExportXmlService
     static function build($arr)
     {
 
+        // Header
         $xml = "<export_order>";
 
+        // fill the data to xml
         foreach ($arr as $data) {
             $xml .= '<order order_id="'.$data[0].'">';
             $xml .= '<order_datetime>' . $data[1] . '</order_datetime>';
@@ -21,11 +23,14 @@ class ExportXmlService
         }
         $xml .= "</export_order>";
 
+        // create XML structure
         $sxe = new \SimpleXMLElement($xml);
         $dom = new \DOMDocument('1,0');
         $dom->preserveWhiteSpace = false;
         $dom->formatOutput = true;
         $dom->loadXML($sxe->asXML());
+
+        // save it to export folder
         $dom->save(__DIR__ . '/../../../var/export/xml/export_order_' . date('YmdHis') . '.xml');
     }
 }
